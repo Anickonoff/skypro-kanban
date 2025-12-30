517ad1db0798943252d011334ed1822f02c52526
Nikonov Anton <Anick.on.off@gmail.com>
2025-12-30 10:39:50 +0700

    add style component for main

---
+++ src/components/Main/Main.styled.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+const StyledMain = styled.main`
+  width: 100%;
+  background-color: #eaeef6;
+`;
+
+const MainBlock = styled.div`
+  max-width: 1260px;
+  width: 100%;
+  margin: 0 auto;
+  padding: 25px 30px 49px;
+`;
+
+const MainContent = styled.div`
+  width: 100%;
+  display: flex;
+`;
+
+export {
+  StyledMain,
+  MainBlock,
+  MainContent
+}
