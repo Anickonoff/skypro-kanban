8c49a26ff636d5fe18b57f412bb2b7d6636c0c88
Anton Nikonov <anick.on.off@gmail.com>
2026-01-09 20:02:53 +0700

    fix error from eslint

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -6,35 +6,30 @@ import ViewCardPage from "./pages/ViewCardPage";
 import SignOutPage from "./pages/SignOutPage";
 import SignInPage from "./pages/SignInPage";
 import SignUpPage from "./pages/SignUpPage";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import PrivateRoute from "./PrivateRoute";
-import Loader from "./components/Loader/Loader";
 
 const AppRoutes = () => {
-  const [user, setUser] = useState(null);
-  const [booting, setBooting] = useState(true);
+  const [user, setUser] = useState(() => {
+    const raw = localStorage.getItem("user");
+    if (raw) {
+      try {
+        return JSON.parse(raw);
+      } catch {
+        localStorage.removeItem("user");
+        return null;
+      }
+    } else {
+      return null;
+    }
+  });
 
   const logout = () => {
     localStorage.removeItem("user");
     setUser(null);
   };
 
-  useEffect(() => {
-    const raw = localStorage.getItem("user");
-    if (raw) {
-      try {
-        setUser(JSON.parse(raw));
-      } catch {
-        localStorage.removeItem("user");
-      }
-    }
-    setBooting(false);
-  }, []);
   const isAuth = !!user;
-  console.log(user);
-  if (booting) {
-    return <Loader />;
-  }
 
   return (
     <Routes>
