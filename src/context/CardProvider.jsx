68a5bcf4b2e3af8d00338385a592ab4aa2e030bb
Anton Nikonov <anick.on.off@gmail.com>
2026-01-12 00:42:15 +0700

    create CardProvider

---
+++ src/context/CardProvider.jsx
@@ -0,0 +1,111 @@
+import { useCallback, useContext, useEffect, useState } from "react";
+import { AuthContext } from "./AuthContext";
+import { createTask, deleteTask, getTasks, updateTask } from "../services/api";
+import { CardContext } from "./CardContext";
+
+export const CardProvider = ({ children }) => {
+  const [loading, setLoading] = useState(false);
+  const [addLoading, setAddLoading] = useState(false);
+  const [editLoading, setEditLoading] = useState(false);
+  const [cards, setCards] = useState([]);
+  const [getError, setGetError] = useState("");
+  const [addError, setAddError] = useState("");
+  const [editError, setEditError] = useState("");
+  const { user } = useContext(AuthContext);
+
+  const getCards = useCallback(async () => {
+    if (!user) {
+      return;
+    }
+    try {
+      setGetError("");
+      setLoading(true);
+      const data = await getTasks({
+        // token: "asb4c4boc86gasb4c4boc86g37w3cc3bo3b83k4g37k3bk3cg3c03ck4k",
+        token: user.token,
+      });
+      if (data) setCards(data);
+    } catch (e) {
+      setGetError(e.message);
+    } finally {
+      setLoading(false);
+    }
+  }, [user]);
+
+  useEffect(() => {
+    if (!user) {
+      setCards([]);
+      setGetError("");
+      setEditError("");
+      setAddError("");
+      return;
+    }
+    getCards();
+  }, [user, getCards]);
+
+  const addCard = async ({ card }) => {
+    if (!user) {
+      return;
+    }
+    try {
+      setAddError("");
+      setAddLoading(true);
+      const newCards = await createTask({ token: user?.token, card });
+      setCards(newCards);
+    } catch (e) {
+      setAddError(e.message);
+    } finally {
+      setAddLoading(false);
+    }
+  };
+
+  const updateCard = async ({ card }) => {
+    if (!user) {
+      return;
+    }
+    try {
+      setEditError("");
+      setEditLoading(true);
+      const newCards = await updateTask({ token: user?.token, card });
+      setCards(newCards);
+    } catch (e) {
+      setEditError(e.message);
+    } finally {
+      setEditLoading(false);
+    }
+  };
+
+  const deleteCard = async ({ id }) => {
+    if (!user) {
+      return;
+    }
+    try {
+      setEditError("");
+      setEditLoading(true);
+      const newCards = await deleteTask({ token: user?.token, id });
+      setCards(newCards);
+    } catch (e) {
+      setEditError(e.message);
+    } finally {
+      setEditLoading(false);
+    }
+  };
+  return (
+    <CardContext.Provider
+      value={{
+        cards,
+        addCard,
+        deleteCard,
+        updateCard,
+        loading,
+        getError,
+        addError,
+        editError,
+        addLoading,
+        editLoading,
+      }}
+    >
+      {children}
+    </CardContext.Provider>
+  );
+};
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -3,42 +3,13 @@ import Wrapper from "../components/Wrapper/Wrapper";
 import Header from "../components/Header/Header";
 import Loader from "../components/Loader/Loader";
 import Main from "../components/Main/Main";
-import { getTasks } from "../services/api";
-import { useCallback, useContext, useEffect, useState } from "react";
-import { AuthContext } from "../context/AuthContext";
 
 const MainPage = () => {
-  const [loading, setLoading] = useState(false);
-  const [cards, setCards] = useState([]);
-  const [error, setError] = useState("");
-  const {user} = useContext(AuthContext);
-  const getCards = useCallback(async () => {
-    if (!user) {
-      return;
-    }
-    try {
-      setError("");
-      setLoading(true);
-      const data = await getTasks({
-        // token: "asb4c4boc86gasb4c4boc86g37w3cc3bo3b83k4g37k3bk3cg3c03ck4k",
-        token: user.token,
-      });
-      if (data) setCards(data);
-    } catch (e) {
-      setError(e.message);
-    } finally {
-      setLoading(false);
-    }
-  }, [user]);
-
-  useEffect(() => {
-    getCards();
-  }, [getCards]);
 
   return (
     <Wrapper>
-      <Header user={user} />
-      {loading ? <Loader /> : <Main cards={cards} error={error} />}
+      <Header />
+      {loading ? <Loader /> : <Main />}
       <Outlet />
     </Wrapper>
   );
