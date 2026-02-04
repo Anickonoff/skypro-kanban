bea91ebf4b980bfb6b1d2eb12bba46167666f664
Anton Nikonov <anick.on.off@gmail.com>
2026-02-04 16:14:05 +0700

    add react-toastify to project

---
+++ src/components/Notifications/Notifications.jsx
@@ -0,0 +1,122 @@
+import { useContext, useEffect, useRef } from "react";
+import { toast, ToastContainer } from "react-toastify";
+import { ThemeModeContext } from "../../context/ThemeModeContext";
+import { TaskContext } from "../../context/TaskContext";
+
+const Notifications = () => {
+  const { theme } = useContext(ThemeModeContext);
+  const {
+    addError,
+    editError,
+    deleteError,
+    addLoading,
+    editLoading,
+    deleteLoading,
+  } = useContext(TaskContext);
+  const toastIdAdd = useRef(null);
+  const toastIdEdit = useRef(null);
+  const toastIdDelete = useRef(null);
+  useEffect(() => {
+    if (addLoading) {
+      toastIdAdd.current = toast.info("Добавление задачи...", {
+        position: "bottom-right",
+        autoClose: false,
+        hideProgressBar: true,
+        closeOnClick: false,
+        pauseOnHover: false,
+        draggable: false,
+        icon: false,
+      });
+    } else {
+      addError
+        ? toast.update(toastIdAdd.current, {
+            render: `Ошибка при добавлении задачи: ${addError}`,
+            type: "error",
+            hideProgressBar: false,
+            autoClose: 3000,
+            icon: true,
+          })
+        : toast.update(toastIdAdd.current, {
+            render: "Задача успешно добавлена!",
+            type: "success",
+            hideProgressBar: false,
+            autoClose: 3000,
+            icon: true,
+          });
+    }
+  }, [addLoading, addError]);
+
+  useEffect(() => {
+    if (editLoading) {
+      toastIdEdit.current = toast.info("Обновление задачи...", {
+        position: "bottom-right",
+        autoClose: false,
+        hideProgressBar: true,
+        closeOnClick: false,
+        pauseOnHover: false,
+        draggable: false,
+        icon: false,
+      });
+    } else {
+      editError
+        ? toast.update(toastIdEdit.current, {
+            render: `Ошибка при обновлении задачи: ${editError}`,
+            type: "error",
+            hideProgressBar: false,
+            autoClose: 3000,
+            icon: true,
+          })
+        : toast.update(toastIdEdit.current, {
+            render: "Задача успешно обновлена!",
+            type: "success",
+            hideProgressBar: false,
+            autoClose: 3000,
+            icon: true,
+          });
+    }
+  }, [editLoading, editError]);
+  useEffect(() => {
+    if (deleteLoading) {
+      toastIdDelete.current = toast.info("Удаление задачи...", {
+        position: "bottom-right",
+        autoClose: false,
+        hideProgressBar: true,
+        closeOnClick: false,
+        pauseOnHover: false,
+        draggable: false,
+        icon: false,
+      });
+    } else {
+      deleteError
+        ? toast.update(toastIdDelete.current, {
+            render: `Ошибка при удалении задачи: ${deleteError}`,
+            type: "error",
+            hideProgressBar: false,
+            autoClose: 3000,
+            icon: true,
+          })
+        : toast.update(toastIdDelete.current, {
+            render: "Задача успешно удалена!",
+            type: "success",
+            hideProgressBar: false,
+            autoClose: 3000,
+            icon: true,
+          });
+    }
+  }, [deleteLoading, deleteError]);
+
+  return (
+    <ToastContainer
+      position="bottom-right"
+      hideProgressBar={false}
+      newestOnTop={true}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      pauseOnHover
+      theme={theme}
+    />
+  );
+};
+
+export default Notifications;
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import Wrapper from "../components/Wrapper/Wrapper";
 import Header from "../components/Header/Header";
 import Main from "../components/Main/Main";
+import Notifications from "../components/Notifications/Notifications";
 
 const MainPage = () => {
 
@@ -11,7 +12,9 @@ const MainPage = () => {
       <Header />
       <Main />
       <Outlet />
+      <Notifications />
     </Wrapper>
+    
   );
 };
 
